6e2c892dbe727e19f2f7713e862d140738c21a24
abj32 <abj32@terpmail.umd.edu>
2025-07-16 18:18:15 -0400

    Change header layout and website logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,17 +14,23 @@ function App() {
   return (
     <BrowserRouter>
       <div className="min-h-screen bg-indigo-50 flex flex-col">
-        <header className="bg-indigo-600 text-white flex flex-col items-center justify-center" style={{ height: '20vh' }}>
-          <nav className="flex gap-6 mb-4">
-            <NavLink to="/" onClick={handleHome} className="font-bold text-xl">🎬 Movie Watchlist</NavLink>
-            <NavLink to="/watchlist" className="hover:underline text-xl">My Watchlist</NavLink>
-          </nav>
-          <div className="w-full max-w-xl px-4">
+        <header className="relative h-[15vh] bg-indigo-600 text-white p-3 flex flex-row items-center">
+          <NavLink to="/" onClick={handleHome} className="text-5xl font-bold tracking-wide" style={{ fontFamily: "'Lilita_One', cursive, sans-serif" }}>
+            🎥ReelSearch
+          </NavLink>
+
+          <div className="absolute left-1/2 transform -translate-x-1/2 w-full max-w-[600px]">
             <SearchBar setResults={setResults} />
+          </div>
+
+          <div className="absolute left-1/2 transform translate-x-[320px]">
+            <NavLink to="/watchlist" className="hover:underline text-xl">
+              My Watchlist
+            </NavLink>
           </div>
         </header>
 
-        <main className="p-4 max-w-4xl mx-auto flex-grow">
+        <main className="p-6">
           <Routes>
             <Route path="/" element={<Home results={results} />} />
             <Route path="/watchlist" element={<Watchlist />} />
