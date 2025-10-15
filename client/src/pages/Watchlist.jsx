6e4f6cdb2bc19e5a5cc93df2a9e6a3033e714686
abj32 <abj32@terpmail.umd.edu>
2025-10-15 19:55:13 -0400

    Add details on hover to items in watchlist

--- client/src/pages/Watchlist.jsx
+++ client/src/pages/Watchlist.jsx
@@ -6,14 +6,13 @@ export default function Watchlist({ watchlist }) {
       {watchlist.length > 0 && (
         <ul className="mt-3">
           {watchlist.map((movie) => (
-
-            <li key={movie.imdbID} className="flex items-center m-[1%] px-[1%] py-[.5%] bg-white rounded-lg shadow-md">
+            <li key={movie.imdbID} className="group relative m-[1%] my-[2%] md:my-[1.5%] xl:my-[1%] bg-white rounded-lg shadow-md">
               {/* Top bar displays movie\show title, year, type, age rating, and genres */}
-              {/* Title */}
-              <h3 className="m-[.5%] text-sm sm:text-base lg:text-lg font-medium">{movie.Title}</h3>
-              {/* Year */}
-              <p className="m-[.5%] ml-[1.5%] text-xs sm:text-sm lg:text-base text-gray-500">{movie.Year}</p>
-              {/* Tags */}
+              <div className="flex items-center px-[1%] py-[.5%]">
+                {/* Title */}
+                <h3 className="m-[.5%] max-w-[45%] text-sm sm:text-base lg:text-lg font-medium">{movie.Title}</h3>
+                {/* Year */}
+                <p className="m-[.5%] ml-[1.5%] text-xs sm:text-sm lg:text-base text-gray-500">{movie.Year}</p>
 
                 {/* Entertainment Type */}
                 <span
@@ -29,9 +28,45 @@ export default function Watchlist({ watchlist }) {
                 </span>
                 {/* Genres */}
                 <span className="flex m-[.6%] md:m-[.5%] l:m-[.4%] xl:m-[.3%] p-[.3%] px-[.6%] text-[5px] sm:text-[6px] md:text-[7px] lg:text-[8px] xl:text-[9px] font-semibold text-gray-700 bg-gray-200 border border-gray-300 rounded">{movie.Genre}</span>
+              </div>
 
               {/* Hover panel displays movie poster, plot, director, actors, and critic ratings */}
-              {/********* TODO ********************/}
+              <div className="relative hidden group-hover:flex items-start p-[1.5%] w-full bg-gray-100 after:content-[''] after:absolute after:inset-x-0 after:top-full after:h-3 sm:after:h-4 lg:after:h-5">
+                {/* Movie Poster */}
+                <img
+                  className="w-[17%] rounded"
+                  src={movie.Poster !== "N/A" ? movie.Poster : "https://placehold.co/300x412?text=No+Image"}
+                  alt={movie.Title}
+                  onError={(e) => {
+                    e.target.onerror = null;
+                    e.target.src = "https://placehold.co/300x412?text=No+Image";
+                  }}
+                />
+
+                <div className="mx-[1.5%]">
+                  {/* Movie Plot */}
+                    <p className="text-xs sm:text-sm lg:text-base"><strong>Plot</strong></p>
+                    <p className="text-xs sm:text-sm lg:text-base text-gray-700">{movie.Plot}</p>
+                  
+                  <div className="mt-[1%] text-xs md:text-sm xl:text-base text-gray-700">
+                    {/* Movie Director */}
+                    <p><strong>Director:</strong> {movie.Director}</p>
+                    {/* Actors/Actresses */}
+                    <p><strong>Actors:</strong> {movie.Actors}</p>
+                  </div>
+
+                  {/* Ratings */}
+                  <p className="mt-[2%] text-xs md:text-sm xl:text-base"><strong>Ratings</strong></p>
+                  <div className="flex text-xs md:text-sm xl:text-base text-gray-700">
+                    {/* IMDB Score */}
+                    <p><strong>IMDB:</strong> {movie.Ratings[0].Value}</p>
+                    {/* Rotten Tomatoes Score */}
+                    <p className="ml-[3%]"><strong>Rotten Tomatoes:</strong> {movie.Ratings[1].Value}</p>
+                    {/* Metacritic Score */}
+                    <p className="ml-[3%]"><strong>Metacritic:</strong> {movie.Ratings[2].Value}</p>
+                  </div>
+                </div>
+              </div>
             </li>
           ))}
         </ul>
